6d1f6da8de61d88e684ce0e39d3ed715a54eb900
avinashk447 <avinashk447@gmail.com>
2019-09-16 18:51:04 +0530

    this is new

--- sample.sh
+++ sample.sh
@@ -2,3 +2,11 @@
 
 echo hello world
 printf "hello word"
+
+#do u know echo escape characters?
+#if u want to give space to one line to other u  
+ echo -e "firstline\nsecond line"
+ # \n - used for second line 
+echo -e "this is the new code of line\t\t\tthis is mycode"
+
+ 

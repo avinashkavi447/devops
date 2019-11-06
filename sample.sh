7c7f943aa7366eed9887dc0b8d6d93d52b3f4638
avinashk444 <43347261+avinashk444@users.noreply.github.com>
2019-11-06 08:19:20 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -5,6 +5,7 @@ printf "hello word"
 
 #do u know echo escape characters?
 #if u want to give space to one line to other u  
+#this is well known script
  echo -e "firstline\nsecond line"
  # \n - used for second line 
 echo -e "this is the new code of line\t\t\tthis is mycode"
